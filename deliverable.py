ff2bfde2d607024bdb516eb145fa032e96b2587c
pambonian <pambonian@gmail.com>
2022-01-13 19:30:56 -0500

    progress

---
+++ deliverable.py
@@ -0,0 +1,64 @@
+# 4 + 4 / 2
+print(4 + 4 / 2)
+# (2 + -7) * 3
+print((2 + -7) * 3)
+# 101 % 10
+print(101 % 10)
+# 12 - 4 % 3
+print(12 - 4 % 3)
+# true && false
+print(True and False)
+# true && !(false || false)
+print(True and not (False or False))
+# !true && !(false || false)
+print(not True and not (False or False))
+# 'cat' + 'dog'
+print('cat' + 'dog')
+# 2 + 'pizza'
+print(str(2) + 'pizza')
+# 2.5 * 'fish'
+print(2.5 * len('fish'))
+# 5 >= 11
+print(5 >= 11)
+# 5 === 5.0
+print()
+# 7 !== 7.1
+print()
+# 5 + 5 > 8
+print()
+# 6 + 6 !== 12
+print()
+# 2 > 1 || false
+print()
+# 'true' === true
+print()
+# 10 % 2 === 0
+print()
+# 21 % 2 === 0
+print()
+# 21 % 2 !== 0
+print()
+# 21 % 2 === 1
+print()
+# 12 % 3 === 0
+print()
+# 9 % 3 === 0
+print()
+# 14 % 3 === 0
+print()
+# 'new york'[0]
+print()
+# 'new york'[1]
+print()
+# 'san francisco'[2 * 3]
+print()
+# 'engineering'[3].toUpperCase()
+print()
+# 'engineering'.indexOf('G')
+print()
+# 'engineering'.indexOf('neer')
+print()
+# 'engineering'.indexOf('r') > -1
+print()
+# 'science'.indexOf('x') === -1
+print()
