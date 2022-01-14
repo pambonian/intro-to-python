f3f160c98dbdd80c6c126077ee19169f99343b4f
pambonian <pambonian@gmail.com>
2022-01-13 19:41:04 -0500

    taskone

--- deliverable.py
+++ deliverable.py
@@ -21,44 +21,44 @@ print(2.5 * len('fish'))
 # 5 >= 11
 print(5 >= 11)
 # 5 === 5.0
-print()
+print(5 == 5.0)
 # 7 !== 7.1
-print()
+print(7 != 7.1)
 # 5 + 5 > 8
-print()
+print(5 + 5 > 8)
 # 6 + 6 !== 12
-print()
+print(6 + 6 != 12)
 # 2 > 1 || false
-print()
+print(2 > 1)
 # 'true' === true
-print()
+print('true' == True)
 # 10 % 2 === 0
-print()
+print(10 % 2 == 0)
 # 21 % 2 === 0
-print()
+print(21 % 2 == 0)
 # 21 % 2 !== 0
-print()
+print(21 % 2 != 0)
 # 21 % 2 === 1
-print()
+print(21 % 2 == 1)
 # 12 % 3 === 0
-print()
+print(12 % 3 == 0)
 # 9 % 3 === 0
-print()
+print(9 % 3 == 0)
 # 14 % 3 === 0
-print()
+print(14 % 3 == 0)
 # 'new york'[0]
-print()
+print('new york'[0])
 # 'new york'[1]
-print()
+print('new york'[1])
 # 'san francisco'[2 * 3]
-print()
+print('san francisco'[2 * 3])
 # 'engineering'[3].toUpperCase()
-print()
+print(('engineering'[3]).upper())
 # 'engineering'.indexOf('G')
-print()
+print("engineering".index("g"))
 # 'engineering'.indexOf('neer')
-print()
+print("engineering".index("neer"))
 # 'engineering'.indexOf('r') > -1
-print()
+print(("engineering".index("r")) > 1)
 # 'science'.indexOf('x') === -1
-print()
+print("science".index("x") == 1)
