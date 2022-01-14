aab0d32c355bdc649c2e8fd9f4bc33e5e102a691
pambonian <pambonian@gmail.com>
2022-01-13 19:42:06 -0500

    added tasktwo questions

--- deliverable.py
+++ deliverable.py
@@ -1,3 +1,5 @@
+#  TASK ONE
+
 # 4 + 4 / 2
 print(4 + 4 / 2)
 # (2 + -7) * 3
@@ -62,3 +64,33 @@ print("engineering".index("neer"))
 print(("engineering".index("r")) > 1)
 # 'science'.indexOf('x') === -1
 print("science".index("x") == 1)
+
+#  TASK TWO
+
+# // 1
+# let idx = 'abcde'.indexOf('D');
+# idx = idx + 11;
+# console.log(idx); // ?
+# idx * 2;
+# console.log(idx); // ?
+
+# // 2
+# let num = 33;
+# let isEven = num % 2 === 0;
+# console.log(isEven); // ?
+# console.log(!isEven); // ?
+
+# // 3
+# let str1 = 'marker';
+# let num = 'whiteboard'.length - str1.length;
+# console.log(num); // ?
+# let str2 = 'bootcamp';
+# console.log(str2[num].toUpperCase()); // ?
+# let char = str2[num].toLowerCase(); // ?
+# console.log(char + '!'); // ?
+
+# // 4
+# let sentence = 'welcome to bootcamp prep';
+# let lastChar = sentence[sentence.length - 1];
+# console.log(lastChar); // ?
+# console.log(sentence.indexOf(lastChar)); // ?
