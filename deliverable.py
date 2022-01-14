a0f81c5dd504857e5289757109db98ebf6d2fc08
pambonian <pambonian@gmail.com>
2022-01-13 19:44:53 -0500

    all questions added

--- deliverable.py
+++ deliverable.py
@@ -94,3 +94,136 @@ print("science".index("x") == 1)
 # let lastChar = sentence[sentence.length - 1];
 # console.log(lastChar); // ?
 # console.log(sentence.indexOf(lastChar)); // ?
+
+# TASK THREE
+
+# // 5
+# let age = 30; // try different numbers here
+# if (age > 30) {
+#   console.log('older than 30');
+# } else {
+#   console.log('younger than 30');
+# }
+
+# // 6
+# let str = 'pizza'; // try different strings here
+# if (str.length > 10) {
+#   console.log('long string');
+# } else {
+#   console.log('short string');
+# }
+# if (str[0] === 'p') {
+#   console.log('starts with p');
+# }
+
+# // 7
+# let num = 15; // try different numbers here
+# if (num % 3 === 0) {
+#   console.log('divisible by 3');
+# } else if (num % 5 === 0) {
+#   console.log('divisible by 5');
+# }
+
+# // 8
+# let num = 15; // try different numbers here
+# if (num % 3 === 0) {
+#   console.log('divisible by 3');
+# }
+# if (num % 5 === 0) {
+#   console.log('divisible by 5');
+# }
+
+# // 9
+# let str = 'General Assembly'; // try different strings here
+# if (str[0] === str[0].toUpperCase()) {
+#   console.log('starts with a capital!');
+# }
+# if (str[str.length - 1] === str[str.length - 1].toUpperCase()) {
+#   console.log('ends with a capital!');
+# }
+
+# // 10
+# let num = -44; // try different numbers here
+# if (num > 0) {
+#   console.log('positive');
+# } else if (num < 0) {
+#   console.log('negative');
+# } else {
+#   console.log(0);
+# }
+# if (num % 2 === 0) {
+#   console.log('even');
+# } else {
+#   console.log('odd');
+# }
+
+# TASK FOUR
+
+# let num = 11; // feel free to change the value of this variable
+# if (num > 5) {
+#   console.log('if');
+# }
+
+# let num = 5; // feel free to change the value of this variable
+# if (num > 5) {
+#   console.log('if');
+# } else {
+#   console.log('else');
+# }
+
+# let num = 0; // feel free to change the value of this variable
+# if (num < 0) {
+#   console.log('if');
+# } else if (num > 0) {
+#   console.log('else if');
+# } else {
+#   console.log('else');
+# }
+
+# TASK FIVE
+
+# function sayHello(name) {
+#     let msg = 'Hello, ' + name + '. How are you?';
+#     return msg;
+#   }
+#   console.log(sayHello('bootcamp prep')); // feel free to change the string being passed
+
+
+# function checkNumber(num) {
+#     if (num > 0) {
+#       return 'positive';
+#     } else if (num < 0) {
+#       return 'negative';
+#     } else {
+#       return 'zero';
+#     }
+#   }
+#   console.log(checkNumber(5)); // feel free to change the number being passed
+
+#   function fizzBuzz1(max) {
+#     for (let i = 0; i < max; i += 1) {
+  
+#       if (i % 3 === 0 && i % 5 !== 0) {
+#         console.log(i);
+#       } else if (i % 5 === 0 && i % 3 !== 0) {
+#         console.log(i);
+#       }
+#     }
+#   }
+
+#   function evenCaps(sentence) {
+#     let newSentence = "";
+  
+#     for (let i = 0; i < sentence.length; i++) {
+#       let char = sentence[i];
+  
+#       if (i % 2 === 0) {
+#         let capitalChar = char.toUpperCase();
+#         newSentence += capitalChar;
+#       } else {
+#         newSentence += char;
+#       }
+#     }
+  
+#     return newSentence;
+#   }
